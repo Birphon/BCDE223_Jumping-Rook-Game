5bbb9044a42722db9ebe815465a0849448abfd97
Birphon <jaredireland1205@gmail.com>
2022-06-23 14:31:02 +1200

    Renamed the levels - not dynamically though also not making the levels dynamic

--- app/src/main/java/com/example/jumpingrook/ToDo_File.java
+++ app/src/main/java/com/example/jumpingrook/ToDo_File.java
@@ -2,11 +2,34 @@ package com.example.jumpingrook;
 
 public class ToDo_File {
 
-    // TODO
-    // TODO: Generate Map
-    // TODO: Generate Player
-    // TODO: Move Player around
-    // TODO: Make Win Game
-    // TODO: Make it so player can only move to the selected number
+    /*
+    TODO: Display the starting level
+    TODO: Display the level name
+    TODO:  Display the goal(s)
+    TODO:  Display the transparent image of player character
+    TODO:  Click event handler or up control button to move player character
+    TODO:  Click event handler or down control button to move player character
+    TODO:  Click event handler or left control button to move player character
+    TODO:  Click event handler or right control button to move player character
+    TODO:  Show the corresponding rules if a move does not obey the move logic
+    TODO:  Display a dialogue if player character has won or lost
+    TODO: Display the number of goals for accomplishment out of the total number of goals
+    TODO: Only update the changed parts of the level when playing the game
+    TODO:  Allow the user to select and load the initial status of a level from a file or database
+    TODO:  Display an updated move count for player character
+    TODO:  Display a GUI element to control sound on and off
+    TODO:  Play a relevant audio if player character has won
+    TODO:  Play a relevant audio if player character intends to move incorrectly
+    TODO:  A button or menu to restart the current maze
+    TODO:  Display a dialogue with options if player character has won or lost
+    TODO:  Play a short relevant video to explain the rules of the game
+    TODO: Calculate and display the elapsed time
+    TODO: Pause a game to stop the timer and hide the level, and un-pause the game to continue the timer and show the level again
+    TODO:  Support ‘undo’ action
+    TODO:  Provide the playback feature of a game
+    TODO:  Allow the user to save a game status in a file or database during playing
+    TODO:  Allow the user to select and load a status of a previous unfinished game from a file or database to continue to play it
+    TODO:  Automatically scale the graphical user interface of the application on devices with different screen sizes
+     */
 
 }
