a5d5f6b3365caae2826140c5f4fd048659d1d0c8
Birphon <jaredireland1205@gmail.com>
2022-06-23 12:32:43 +1200

    Added ToDO list Going to get lunch

---
+++ app/src/main/java/com/example/jumpingrook/ToDo_File.java
@@ -0,0 +1,12 @@
+package com.example.jumpingrook;
+
+public class ToDo_File {
+
+    // TODO
+    // TODO: Generate Map
+    // TODO: Generate Player
+    // TODO: Move Player around
+    // TODO: Make Win Game
+    // TODO: Make it so player can only move to the selected number
+
+}
