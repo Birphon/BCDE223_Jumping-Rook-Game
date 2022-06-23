1f3dd22a8e6a4522a95a7f7e0d6dbf456eeccbd3
Birphon <jaredireland1205@gmail.com>
2022-06-23 12:21:21 +1200

    Made a Level Selector to go to Level 1 and Level 2

--- app/src/main/java/com/example/jumpingrook/MainActivity.java
+++ app/src/main/java/com/example/jumpingrook/MainActivity.java
@@ -6,26 +6,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     // This is the main Menu
 
-    public static final String EXTRA_MESSAGE = "com.example.jumpingrook.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void toLevelOne(View view){
-        Intent intent = new Intent(this, level_one.class);
+    public void goToLevelOne (View view){
+        Intent intent = new Intent (this, level_one.class);
         startActivity(intent);
     }
 
-    public void toLevelTwo(View view){
-        Intent intent = new Intent(this, level_two.class);
+    public void goToLevelTwo (View view){
+        Intent intent = new Intent (this, level_two.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/jumpingrook/level_one.java
+++ app/src/main/java/com/example/jumpingrook/level_one.java
@@ -2,7 +2,6 @@ package com.example.jumpingrook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class level_one extends AppCompatActivity {
@@ -11,7 +10,5 @@ public class level_one extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
-
-        Intent intent = getIntent();
     }
 }
